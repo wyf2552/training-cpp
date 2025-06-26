088e643407dd21cc7a3a59ccc5ba31302d59cd08
wyf2552 <wyf5322@gmail.com>
2025-06-26 22:44:46 +0800

    00_hello word

--- exercises/00_hello_world/main.cpp
+++ exercises/00_hello_world/main.cpp
@@ -7,6 +7,6 @@
 int main(int argc, char **argv) {
     // TODO: 在控制台输出 "Hello, 希加加训练营!" 并换行
     // 下文中的代码如果无法通过编译，请自行修改代码
-    std::cout : "Hello, 希加加训练营!" + std::endl;
+    std::cout << "Hello, 希加加训练营!" << std::endl;
     return 0;
 }
