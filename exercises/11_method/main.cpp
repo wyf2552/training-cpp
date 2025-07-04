1f2831972511a536f3bc259d98055fa6d0814ee2
wyf2552 <wyf5322@gmail.com>
2025-07-04 23:44:14 +0800

    11 method

--- exercises/11_method/main.cpp
+++ exercises/11_method/main.cpp
@@ -6,8 +6,15 @@ struct Fibonacci {
 
     // TODO: 实现正确的缓存优化斐波那契计算
     unsigned long long get(int i) {
-        for (; false; ++cached) {
-            cache[cached] = cache[cached - 1] + cache[cached - 2];
+        if (i >= 128) {
+            return 0;
+        }
+        
+        if (i >= cached) {
+            for (int j = cached; j <= i; j++) {
+                cache[j] = cache[j - 1] + cache[j - 2];
+            }
+            cached = i + 1;
         }
         return cache[i];
     }
@@ -15,7 +22,10 @@ struct Fibonacci {
 
 int main(int argc, char **argv) {
     // TODO: 初始化缓存结构体，使计算正确
-    Fibonacci fib;
+    Fibonacci fib = {
+        {0, 1},
+        2
+    };
     ASSERT(fib.get(10) == 55, "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << fib.get(10) << std::endl;
     return 0;
