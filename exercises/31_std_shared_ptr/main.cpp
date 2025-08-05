9b76936d3321c8490614e5aac8994b4e93a2f945
wyf2552 <wyf5322@gmail.com>
2025-08-06 00:06:53 +0800

    share_ptr

--- exercises/31_std_shared_ptr/main.cpp
+++ exercises/31_std_shared_ptr/main.cpp
@@ -10,36 +10,36 @@ void TestSharedPtr() {
     std::shared_ptr<int> ptrs[]{shared, shared, shared};
 
     std::weak_ptr<int> observer = shared;
-    ASSERT(observer.use_count() == ?, "");
+    ASSERT(observer.use_count() == 4, "");
 
     ptrs[0].reset();
-    ASSERT(observer.use_count() == ?, "");
+    ASSERT(observer.use_count() == 3, "");
 
     ptrs[1] = nullptr;
-    ASSERT(observer.use_count() == ?, "");
+    ASSERT(observer.use_count() == 2, "");
 
     ptrs[2] = std::make_shared<int>(*shared);
-    ASSERT(observer.use_count() == ?, "");
+    ASSERT(observer.use_count() == 1, "");
 
     ptrs[0] = shared;
     ptrs[1] = shared;
     ptrs[2] = std::move(shared);
-    ASSERT(observer.use_count() == ?, "");
+    ASSERT(observer.use_count() == 3, "");
 
     std::ignore = std::move(ptrs[0]);
     ptrs[1] = std::move(ptrs[1]);
     ptrs[1] = std::move(ptrs[2]);
-    ASSERT(observer.use_count() == ?, "");
+    ASSERT(observer.use_count() == 1, "");
 
     shared = observer.lock();
-    ASSERT(observer.use_count() == ?, "");
+    ASSERT(observer.use_count() == 2, "");
 
     shared = nullptr;
     for (auto &ptr : ptrs) ptr = nullptr;
-    ASSERT(observer.use_count() == ?, "");
+    ASSERT(observer.use_count() == 0, "");
 
     shared = observer.lock();
-    ASSERT(observer.use_count() == ?, "");
+    ASSERT(observer.use_count() == 0, "");
 }
 
 // NOTICE: shared_ptr 的循环引用问题
@@ -52,11 +52,15 @@ struct A {
 struct B {
     std::shared_ptr<A> ptr;
     ~B() { std::cout << "B destroyed" << std::endl; }
-}
+};
 
 void TestSharedPtrCycle() {
     // TODO： 写一个测试用例，展示 shared_ptr 的循环引用问题
     // NOTICE：使用 `std::weak_ptr` 可以解决循环引用问题。
+    auto a = std::make_shared<A>();
+    auto b = std::make_shared<B>();
+    a->ptr = b;
+    b->ptr = a;
 }
 
 template<typename T>
@@ -64,7 +68,75 @@ class MySharedPtr {
     // TODO: 实现 shared_ptr 的基本功能
     // NOTICE: 面试时经常会问到 shared_ptr 的实现原理，建议自己独立实现一个简单的 shared_ptr。
     // 例如，构造函数、析构函数、移动构造函数、移动赋值运算符等。
-}
+
+private:
+    T* ptr = nullptr;
+    int* refCount = nullptr;
+
+    void release() {
+        if (refCount && --(*refCount) == 0) {
+            delete ptr;
+            delete refCount;
+        }
+    }
+
+public:
+    explicit MySharedPtr(T* p = nullptr) : ptr(p), refCount(new int(1)) {}
+
+    ~MySharedPtr() { release(); }
+
+    MySharedPtr(const MySharedPtr& other) : ptr(other.ptr), refCount(other.refCount) {
+        if (refCount) {
+            ++(*refCount);
+        }
+    }
+
+    MySharedPtr(MySharedPtr&& other) noexcept : ptr(other.ptr), refCount(other.refCount) {
+        other.ptr = nullptr;
+        other.refCount = nullptr;
+    }
+
+    MySharedPtr& operator=(const MySharedPtr& other) {
+        if (this != &other) {
+            release();
+            ptr = other.ptr;
+            refCount = other.refCount;
+            if (refCount) {
+                ++(*refCount);
+            }
+        }
+        return *this;
+    }
+
+    MySharedPtr& operator=(MySharedPtr&& other) noexcept {
+        if (this != &other) {
+            release();
+            ptr = other.ptr;
+            refCount = other.refCount;
+            other.ptr = nullptr;
+            other.refCount = nullptr;
+        }
+        return *this;
+    }
+
+    int use_count() const {
+        return refCount ? *refCount : 0;
+    }
+
+    T* get() const {
+        return ptr;
+    }
+    T& operator*() const {
+        return *ptr;
+    }
+    T* operator->() const {
+        return ptr;
+    }
+
+    explicit operator bool() const {
+        return ptr != nullptr;
+    }
+};
 
 int main(int argc, char **argv) {
     TestSharedPtr();
@@ -78,8 +150,8 @@ int main(int argc, char **argv) {
     ptr = std::move(ptr3);
 
     // TODO: 将下列 `?` 替换为正确的值
-    ASSERT(ptr.use_count() == ?, "");
-    ASSERT(ptr2.use_count() == ?, "");
-    ASSERT(ptr3.use_count() == ?, "");
+    ASSERT(ptr.use_count() == 2, "");
+    ASSERT(ptr2.use_count() == 2, "");
+    ASSERT(ptr3.use_count() == 0, "");
     return 0;
 }
