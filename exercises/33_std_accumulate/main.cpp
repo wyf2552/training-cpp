9fb875330ac3b30764d1100f188e99d5d8a099c5
wyf2552 <wyf5322@gmail.com>
2025-08-11 23:50:46 +0800

    33 accumulate

--- exercises/33_std_accumulate/main.cpp
+++ exercises/33_std_accumulate/main.cpp
@@ -12,6 +12,8 @@ int main(int argc, char **argv) {
     //       - 连续存储；
     //       的张量占用的字节数
     // int size = ?;
+    int elements = std::accumulate(shape, shape + 4, 1, std::multiplies<int>());
+    int size = elements * sizeof(DataType);
     ASSERT(size == 602112, "4x1x3x224x224 = 602112");
     return 0;
 }
